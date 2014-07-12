e2754ff528fa3e1e9b613eeff7d55824fa21c0cf
IlyaKravchuk <kravilia@rambler.ru>
2014-07-12 15:30:10 +0600

    Add version 1.0

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -15,7 +15,7 @@ namespace ConsoleApplication2
             Console.WriteLine("Введите ординату точки С :");
             Single korY = Convert.ToSingle(Console.ReadLine());
 
-            if (korY >= Math.Abs(korX) - 1 & korY <= -Math.Abs(korX) + 1)
+            if ( korY >= Math.Abs(korX) - 1 & korY <= -Math.Abs(korX) +1 )
                 Console.WriteLine("Заданная точка C (" + korX + ";" + korY + ") принадлежит заштрихованной плоскости");
             else
                 Console.WriteLine("Данная точка C (" + korX + ";" + korY + ") не принадлежит заштрихованной плоскости");
